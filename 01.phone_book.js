9bdcee89a57a54b4ab1e79d44a5e4dd61003726f
lentqq <lentqq@gmail.com>
2019-03-18 08:08:38 +0200

    Add new file

---
+++ 01.phone_book.js
@@ -0,0 +1,14 @@
+function phoneBook(array) {
+    let phoneBook = {};
+
+    for (let i = 0; i < array.length; i++) {
+        let [name, number] = array[i].split(' ');
+        phoneBook[name] = number;
+    }
+
+    for (let name in phoneBook) {
+      console.log(`${name} -> ${phoneBook[name]}`);
+    }
+}
+
+phoneBook(['Tim 0834212554', 'Peter 0877547887', 'Bill 0896543112', 'Tim 0876566344'])
