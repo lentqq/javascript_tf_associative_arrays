f057010ea30392857cb9166b8fec683fe042bcad
lentqq <lentqq@gmail.com>
2019-03-20 07:16:58 +0200

    Editing file

--- 01.phone_book.js
+++ 01.phone_book.js
@@ -9,6 +9,7 @@ function phoneBook(array) {
     for (let name in phoneBook) {
       console.log(`${name} -> ${phoneBook[name]}`);
     }
+
 }
 
 phoneBook(['Tim 0834212554', 'Peter 0877547887', 'Bill 0896543112', 'Tim 0876566344'])
